ab8c4a2e22af9f68bb53796b1777abe923ecb2c7
jivan <jeevanaryal7947@gmail.com>
2024-07-19 12:00:53 +0545

    added prisma schema and also don't forget to open docker before connecting with database

--- src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-// for query purposes
-const queryClient = postgres(process.env.POSTGRES_URL!);
-export const db = drizzle(queryClient);
