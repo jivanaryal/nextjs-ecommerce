ced986dfc4a6ea54289d761f33a92397c245e398
Jivan Aryal <104553708+jivanaryal@users.noreply.github.com>
2024-08-01 22:12:49 +0545

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,6 +6,7 @@ type Props = {};
 const ProductPage = (props: Props) => {
   return (
     <section>
+      <h1> This is the products list where you can see all of the product</h1>
       <ProductsList variant="all-product" />
     </section>
   );
